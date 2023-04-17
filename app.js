0ec78a8b4e61752224d18ce53bcefd3e64073d75
Владислав Тищенко <vladislavtishenko@MacBook-Air-Vladislav.local>
2023-04-17 14:06:15 +0300

    добавил lighting

--- app.js
+++ app.js
@@ -1,88 +1,97 @@
-const axios = require("axios");
-const fs = require("fs");
-const path = require("path");
-const EventEmmiter = require("events");
+import axios from "axios";
+import fs from "fs";
+import path from "path";
+import EventEmmiter from "events";
+import logUpdate from "log-update";
+import chalk from "chalk";
 
-const { Telegram } = require("./lib/Telegram");
+import Telegram from "./lib/Telegram.js";
 
-const { parse } = require("node-html-parser");
-
-const configPath = path.resolve(`${__dirname}/config.txt`);
-
-if (!fs.existsSync(configPath)) return console.log("Не найден файл config.txt");
-
-const configContent = fs.readFileSync(configPath, "utf8");
-
-if (!configContent) return console.log("Файл config.txt пуст");
-
-let list;
-
-if (/\r/.test(configContent)) {
-	list = configContent.split("\r");
-} else {
-	list = configContent.split("\n");
-}
-
-if (!list.length) return console.log("Файл config.txt пуст");
-
-let config = {};
-let cardsLength = 0;
-let token = null;
-let chatId = null;
-let timeout = null;
-
-for (const string of list) {
-	try {
-		if (string.includes("Токен")) {
-			token = string.split(":")[1] + ":" + string.split(":")[2];
-		} else if (string.includes("Чата")) {
-			chatId = string.split(":")[1];
-		} else if (string.includes("Таймаут")) {
-			timeout = string.split(":")[1];
-		} else {
-			let row = string.split(";");
-
-			let colName = row[0].split(":")[1];
-			let colCards = row[1].split(":")[1].split(",");
-			let colPercents = row[2].split(":")[1].split(",");
-
-			config[colName] = {};
-
-			if (colName === "Monopoly") {
-				config[colName].url = "https://tracksino.com/monopoly";
-			} else if (colName === "Crazy Time") {
-				config[colName].url = "https://tracksino.com/crazytime";
-			} else if (colName === "Dream Catcher") {
-				config[colName].url = "https://tracksino.com/dreamcatcher";
-			}
-
-			config[colName].cards = [];
-
-			for (let i = 0; i < colCards.length; i++) {
-				const name = colCards[i];
-				const limit = colPercents[i];
-				config[colName].cards.push({ name, limit });
-			}
-
-			cardsLength += config[colName].cards.length;
-		}
-	} catch (err) {
-		console.log(err.message);
-	}
-}
-
-if (!token) return console.log("Не указан телеграм токен");
-
-if (!chatId) return console.log("Не указан ID чата телеграм");
-
-if (!timeout) return console.log("Не указан таймаут");
-
-const telegram = new Telegram(token, chatId);
+import { parse } from "node-html-parser";
 
 //?period=1hour
 //?period=3hour
 
-async function main(config) {
+async function main() {
+	const __dirname = path.resolve();
+	const configPath = path.resolve(`${__dirname}/config.txt`);
+
+	if (!fs.existsSync(configPath))
+		return console.log("Не найден файл config.txt");
+
+	const configContent = fs.readFileSync(configPath, "utf8");
+
+	if (!configContent) return console.log("Файл config.txt пуст");
+
+	let list;
+
+	if (/\r/.test(configContent)) {
+		list = configContent.split("\r");
+	} else {
+		list = configContent.split("\n");
+	}
+
+	if (!list.length) return console.log("Файл config.txt пуст");
+
+	let config = {};
+	// let cardsLength = configContent.includes("Lightning Roulette") ? 1 : 0;
+	let cardsLength = 0;
+	let token = null;
+	let chatId = null;
+	let timeout = null;
+
+	for (const string of list) {
+		try {
+			if (string.includes("Токен")) {
+				token = string.split(":")[1] + ":" + string.split(":")[2];
+			} else if (string.includes("Чата")) {
+				chatId = string.split(":")[1];
+			} else if (string.includes("Таймаут")) {
+				timeout = string.split(":")[1];
+			} else {
+				let row = string.split(";");
+
+				let colName = row[0].split(":")[1];
+
+				config[colName] = {};
+
+				if (colName === "Monopoly") {
+					config[colName].url = "https://tracksino.com/monopoly?period=1hour";
+				} else if (colName === "Crazy Time") {
+					config[colName].url = "https://tracksino.com/crazytime?period=1hour";
+				} else if (colName === "Dream Catcher") {
+					config[colName].url =
+						"https://tracksino.com/dreamcatcher?period=1hour";
+				} else if (colName === "Lightning Roulette") {
+					config[colName].url =
+						"https://tracksino.com/lightning-roulette?period=3hours";
+				}
+
+				let colCards = row[1].split(":")[1].split(",");
+				let colPercents = row[2].split(":")[1].split(",");
+
+				config[colName].cards = [];
+
+				for (let i = 0; i < colCards.length; i++) {
+					const name = colCards[i];
+					const limit = colPercents[i];
+					config[colName].cards.push({ name, limit });
+				}
+
+				cardsLength += config[colName].cards.length;
+			}
+		} catch (err) {
+			console.log(err);
+		}
+	}
+
+	if (!token) return console.log("Не указан телеграм токен");
+
+	if (!chatId) return console.log("Не указан ID чата телеграм");
+
+	if (!timeout) return console.log("Не указан таймаут");
+
+	const telegram = new Telegram(token, chatId);
 	const emitter = new EventEmmiter();
 
 	const games = config;
@@ -90,6 +99,7 @@ async function main(config) {
 
 	emitter.on("complete", async (result) => {
 		if (!result.length) return console.log("Нет данных");
+
 		let resultMessage = "Подходящие карточки:";
 		for (const game in games) {
 			const { url, cards } = games[game];
@@ -100,8 +110,13 @@ async function main(config) {
 			for (let i = 0; i < cards.length; i++) {
 				const card = cards[i];
 
-				const { name, limit } = card;
-				const cardPercent = gameResult.find((item) => item[name])[name];
+				let { name, limit } = card;
+
+				let cardPercent = gameResult.find((item) => item[name])[name];
+
+				limit = parseFloat(limit);
+				cardPercent = parseFloat(cardPercent);
+
 				if (cardPercent < limit) {
 					isCardFinded = true;
 					resultMessage += `\nКарточка: "<b>${name}</b>" Желаемый: <b>${cardPercent}%</b>`;
@@ -119,10 +134,20 @@ async function main(config) {
 			}
 		}
 		if (resultMessage.includes("Карточка")) {
-			console.log("Отправляю результаты в телеграм");
+			// console.log("Отправляю результаты в телеграм");
+			logUpdate(
+				`Запрос данных с сайта: ${chalk.bold.yellow(
+					"tracksino.com",
+				)}, Результат: ${chalk.bold.bgGreen("отправка в телеграм")}`,
+			);
 			await telegram.sendMessage(resultMessage);
 		} else {
-			console.log("Нет результатов");
+			logUpdate(
+				`Запрос данных с сайта: ${chalk.bold.yellow(
+					"tracksino.com",
+				)}, Результат: ${chalk.bold.bgMagenta("не найдено")}`,
+			);
+			// console.log("Нет результатов");
 		}
 	});
 
@@ -150,38 +175,58 @@ async function main(config) {
 
 		try {
 			root = parse(data);
-
-			for (const card of cards) {
-				let target = null;
-
-				card.name.includes("Bonus")
-					? (target = `${card.name} Segment`)
-					: (target = `${game} ${card.name} Segment`);
-
-				const image = root.querySelector(`img[alt="${target}"]`);
-
-				if (!image) throw new Error("Image not found");
-
-				const parent = image.parentNode;
-
-				if (!parent) throw new Error("Parent not found");
-
-				const title = parent.querySelector(".title");
-
-				if (!title) throw new Error("Title not found");
-
-				let percent = title.textContent;
-
-				if (!percent || !percent.includes("%"))
-					throw new Error("Percent not found");
-
-				percent = parseFloat(percent).toFixed(2);
-
-				percents.push({ game, [card.name]: percent });
-
-				if (percents.length === cardsLength) {
-					emitter.emit("complete", percents);
+			if (game === "Lightning Roulette") {
+				const rates = root.querySelectorAll(".rate");
+				let finded = [];
+				rates.pop(); // remove last element
+				for (const rate of rates) {
+					let rateValue = rate.textContent;
+					let percent = parseFloat(rateValue); //finded percent
+					let myPercent = parseFloat(cards[0].limit); // config percent
+					if (percent < myPercent) {
+						finded.push(percent);
+					}
 				}
+
+				if (finded.length) {
+					const value = Math.min(...finded);
+					percents.push({ game, any: value });
+				} else {
+					percents.push({ game, any: "100" });
+				}
+			} else {
+				for (const card of cards) {
+					let target = null;
+
+					card.name.includes("Bonus")
+						? (target = `${card.name} Segment`)
+						: (target = `${game} ${card.name} Segment`);
+
+					const image = root.querySelector(`img[alt="${target}"]`);
+
+					if (!image) throw new Error("Image not found");
+
+					const parent = image.parentNode;
+
+					if (!parent) throw new Error("Parent not found");
+
+					const title = parent.querySelector(".title");
+
+					if (!title) throw new Error("Title not found");
+
+					let percent = title.textContent;
+
+					if (!percent || !percent.includes("%"))
+						throw new Error("Percent not found");
+
+					percent = parseFloat(percent).toFixed(2);
+
+					percents.push({ game, [card.name]: percent });
+				}
+			}
+
+			if (percents.length === cardsLength) {
+				emitter.emit("complete", percents);
 			}
 		} catch (err) {
 			console.log(err.message);
@@ -192,10 +237,15 @@ async function main(config) {
 	console.log("Чекер запущен");
 
 	setInterval(() => {
-		console.log("Запрос данных с сайта: tracksino.com");
+		logUpdate(
+			`Запрос данных с сайта: ${chalk.bold.yellow(
+				"tracksino.com",
+			)}, Результат: ${chalk.bold.bgYellow("ожидайте...")}`,
+		);
+		// console.log("Запрос данных с сайта: tracksino.com");
 		percents = [];
 		makeRequest(games, handleResponse);
 	}, timeout * 1000);
 }
 
-main(config);
+main();
